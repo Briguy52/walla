2fe2fb23b4107b28b58c2da469e6566c1dac5343
Briguy52 <Briguy52@gmail.com>
2016-07-13 23:41:59 -0400

    ConvoCell: updated content to show other persons info by default

--- Walla/ConvoCell.swift
+++ Walla/ConvoCell.swift
@@ -13,23 +13,26 @@ class ConvoCell: UITableViewCell {
 	@IBOutlet weak var profile: UIImageView!
 	@IBOutlet weak var nameLabel: UILabel!
 	@IBOutlet weak var previewLabel: UILabel!
+    
+    let myBasic = Basic()
+    let myUserBackend = UserBackend()
+    let myConvoBackend = ConvoBackend()
 	
 	var convoModel: ConvoModel! {
 		didSet {
             
-            let myUserBackend = UserBackend()
-            myUserBackend.getUserInfo("displayName", userID: convoModel.authorID) {
-                (result: AnyObject) in
-                self.nameLabel.text = result as! String
-            }
+            let userID = self.myBasic.rootRef.authData.uid
+            let otherID = self.myConvoBackend.printNotMe(convoModel, userID: userID)
             
+            self.nameLabel.text = self.myUserBackend.getSenderName(otherID)
+
             let myRequestBackend = RequestBackend()
             myRequestBackend.getRequestValue(convoModel.uniqueID, key: "request") {
                 (result: String) in
                 self.previewLabel.text = result
             }
             
-            myUserBackend.getUserInfo("profilePicUrl", userID: convoModel.authorID) {
+            myUserBackend.getUserInfo("profilePicUrl", userID: otherID) {
                 (result: AnyObject) in
                 self.profile.setImageWithURL(NSURL(string: result as! String)!)
             }
