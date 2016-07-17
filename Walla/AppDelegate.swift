79bd288eac158f72fbcaa921a56eabef5aaf53dc
timchoh585 <timchoh@gmail.com>
2016-07-17 19:38:25 -0400

    new firebase stuff. went through most of the steps.

--- Walla/AppDelegate.swift
+++ Walla/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -18,6 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 		
 //		let lock = MyApplication.sharedInstance.lock
 //		lock.applicationLaunchedWithOptions(launchOptions)
+		
 		
 		return true
 	}
