944a838b8a82ca01aa0dfaa9bf0f2747945bd4d9
Briguy52 <Briguy52@gmail.com>
2016-08-09 18:17:43 -0500

    DONE: added walla logo with rounded corners, TODO: center

--- Walla/InitialViewController.swift
+++ Walla/InitialViewController.swift
@@ -14,10 +14,19 @@ class InitialViewController: UIViewController {
     
     @IBOutlet weak var wallaTitleLabel: UILabel!
     
+    @IBOutlet weak var wallaLogo: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         UIApplication.sharedApplication().statusBarStyle = .LightContent
         // Do any additional setup after loading the view, typically from a nib.
+        formatLogo()
+    }
+    
+    func formatLogo() {
+        self.wallaLogo.layer.cornerRadius = self.wallaLogo.frame.size.width / 3;
+        self.wallaLogo.clipsToBounds = true
+        self.wallaLogo.layer.borderWidth = 3.0
+        self.wallaLogo.layer.borderColor = UIColor.whiteColor().CGColor
     }
     
     override func didReceiveMemoryWarning() {
